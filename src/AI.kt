a1f7c8cedfbbe3b6d2383c547119c1ba74a6cd84
Noor ahmad <NourAhmedAbdulSattar41@gmail.com>
2021-04-08 15:40:55 -0700

    add after improvement (Al && Cell) class in kotlin

--- src/AI.kt
+++ src/AI.kt
@@ -1,6 +1,7 @@
-class AI { //NOT YET USED
-    //easy - will make losing moves
-    //medium - will try to make a tictactoe
-    //hard - will block other player's tictactoe
-    //stupid - will make random moves (easiest to code)
+class AI {
+     /*NOT YET USED*/
+     /*easy - will make losing moves*/
+     /*medium - will try to make a tictactoe*/
+     /*hard - will block other player's tictactoe*/
+     /*stupid - will make random moves (easiest to code)*/
 }
--- src/Cell.kt
+++ src/Cell.kt
@@ -1,18 +1,10 @@
 class Cell {
-    //This is one cell of the game grid
-    var contents = " "
-    @JvmField
-    var empty = true
-    fun output(): String {
-        return contents
-    }
-
+    /*This is one cell of the game grid*/
+    var contents:String = " "
+    var empty:Boolean = true
+    fun output(): String = contents
     fun placeMark() {
-        if (TicTacToe.count % 2 == 0) {
-            contents = "X"
-        } else {
-            contents = "O"
-        }
-        empty = false
+        if (TicTacToe.count % 2 == 0) contents = "X" else contents = "O"
+               empty = false
     }
 }
